05dbff4bb08d58cb3f3235554cf1d1f910d130d5
seb-faull <seb_faull@hotmail.co.uk>
2017-11-16 09:26:32 +0000

    FizzBuzz complete

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,15 +1,27 @@
 <?php
 
-  class FizzBuzz {
+class FizzBuzz {
 
-    function range_numbers() {
-      $array = range(1, 100);
+  function initiate_fizzbuzz() {
+    for ($number_count = 1; $number_count <= 100; $number_count++) {
+      if ($number_count % 3 == 0 && $number_count % 5 == 0) {
+        echo "FizzBuzz ";
+      }
+      elseif ($number_count % 3 == 0) {
+        echo "Fizz ";
+      }
+      elseif ($number_count % 5 == 0) {
+        echo "Buzz ";
+      }
+      else {
+        echo $number_count . ' ';
+      }
     }
-
-    function initiate_fizzbuzz () {
-      range_numbers
-    }
-
   }
+
+}
+
+$test = new FizzBuzz;
+print_r($test->initiate_fizzbuzz());
 
 ?>
